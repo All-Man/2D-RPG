5b6b99c52873e6cb6e0f6e744ca65ef51245b4c4
allespro <arcy.all@yahoo.com>
2019-04-23 23:19:17 +0000

    У врага появились жизни и урон от снарядов

--- KinematicEnemy1.gd
+++ KinematicEnemy1.gd
@@ -2,9 +2,13 @@ extends KinematicBody2D
 
 var IsPlayerNear = false
 var SPEED = 120
+var health = 100
 var PlayerPos = Vector2(0,0)
 
 func _physics_process(delta):
+	$HealthBar.set_value(health)
+	if(health <= 0):
+		queue_free()
 	if(IsPlayerNear == true):
 		PlayerPos.x = $"../../../../KinematicPlayer".global_position.x - self.global_position.x
 		PlayerPos.y = $"../../../../KinematicPlayer".global_position.y - self.global_position.y
@@ -16,6 +20,12 @@ func _physics_process(delta):
 
 func attack():
 	IsPlayerNear = true
-	
 func stop_attack():
 	IsPlayerNear = false
+
+func _on_HitZone_area_entered(area):
+	var groups = area.get_groups()
+	if (groups.has("player")):
+		print("EEEEBOYYY")
+	if (groups.has("bullet")):
+		health -= 10

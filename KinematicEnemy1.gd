5b862b1c9e6de7508d4dc0153c6b4bf77dc5367b
allespro <arcy.all@yahoo.com>
2019-04-24 21:09:02 +0000

    Анимация врага
    Теперь враг правильно реагирует на положение перса. Чекай код в res://KinematicEnemy1.gd в функции rotation_loop()

--- KinematicEnemy1.gd
+++ KinematicEnemy1.gd
@@ -14,13 +14,51 @@ func _physics_process(delta):
 		PlayerPos.y = $"../../../../KinematicPlayer".global_position.y - self.global_position.y
 		var motion = PlayerPos.normalized() * SPEED
 		move_and_slide(motion, Vector2(0,0))
+		
+		rotation_loop()
 
 
+
+
+
+func rotation_loop():
+	if(PlayerPos.x < 0 && PlayerPos.y < 0): # 1 ая четверть окружности
+		if(abs(PlayerPos.y) < abs(PlayerPos.x)):
+			$AnimatedSprite.play("LEFT")
+		elif(abs(PlayerPos.y) > abs(PlayerPos.x)):
+			$AnimatedSprite.play("UP")
+		else:
+			$AnimatedSprite.stop()
+	elif(PlayerPos.x > 0 && PlayerPos.y < 0): # 2 ая четверть окружности
+		if(abs(PlayerPos.y) < abs(PlayerPos.x)):
+			$AnimatedSprite.play("RIGHT")
+		elif(abs(PlayerPos.y) > abs(PlayerPos.x)):
+			$AnimatedSprite.play("UP")
+		else:
+			$AnimatedSprite.stop()
+	elif(PlayerPos.x > 0 && PlayerPos.y > 0): # 3 ая четверть окружности
+		if(abs(PlayerPos.y) < abs(PlayerPos.x)):
+			$AnimatedSprite.play("RIGHT")
+		elif(abs(PlayerPos.y) > abs(PlayerPos.x)):
+			$AnimatedSprite.play("DOWN")
+		else:
+			$AnimatedSprite.stop()
+	elif(PlayerPos.x < 0 && PlayerPos.y > 0): # 4 ая четверть окружности
+		if(abs(PlayerPos.y) > abs(PlayerPos.x)):
+			$AnimatedSprite.play("DOWN")
+		elif(abs(PlayerPos.y) < abs(PlayerPos.x)):
+			$AnimatedSprite.play("LEFT")
+		else:
+			$AnimatedSprite.stop()
+	else:
+		$AnimatedSprite.stop()
 
 
 func attack():
 	IsPlayerNear = true
 func stop_attack():
+	print("-----------------------")
+	$AnimatedSprite.stop()
 	IsPlayerNear = false
 
 func _on_HitZone_area_entered(area):
